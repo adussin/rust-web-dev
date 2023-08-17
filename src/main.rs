f69b63d5bd86f336419a990912ec7c137ac635e1
Alessandro Dussin <alessandro.dussin@gmail.com>
2023-08-17 21:13:14 +0200

    Using State

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,52 @@
+use std::fs;
+
 use axum::{
     response::Html,
     routing::get,
+    extract::{State, Path},
     Router,
+};
+use minijinja::{
+    context, Environment, Error, ErrorKind,
 };
 
 #[tokio::main]
-async fn main() {
+async fn main() {   
+    let mut env = Environment::new();
+    let template_path = std::env::current_dir().unwrap().join("static/templates");
+    //println!("{}", template_path.display());
+    env.set_loader(move |name| {
+        let pieces = name.split('/');
+        let mut path = template_path.clone();
+        for piece in pieces {
+            if piece != "." && piece != ".." && !piece.contains('\\') {
+                path.push(piece);
+            } else {
+                return Ok(None);
+            }
+        }
+
+        match fs::read_to_string(path) {
+            Ok(result) => Ok(Some(result)),
+            Err(err) => {
+                if err.kind() == std::io::ErrorKind::NotFound {
+                    Ok(None)
+                } else {
+                    Err(
+                        Error::new(ErrorKind::TemplateNotFound, "failed to load template")
+                            .with_source(err),
+                    )
+                }
+            }
+        }
+    });
+
+    
     // build our application with a single route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+            .route("/", get(index))
+            .route("/:profile", get(profile))
+            .with_state(env);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -16,6 +55,16 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("Porcodiooooo\n")
+async fn index(State(env): State<Environment<'_>>) -> String {
+    let tmpl = env.get_template("index.html").unwrap();
+    tmpl.render(context! {
+        name => "Diocane"
+    }).unwrap()
+}
+
+async fn profile(State(env): State<Environment<'_>>, Path(profile): Path<String>) -> Html<String> {
+    let tmpl = env.get_template("profile.html").unwrap();
+    Html(tmpl.render(context! {
+        profile => profile
+    }).unwrap())
 }
